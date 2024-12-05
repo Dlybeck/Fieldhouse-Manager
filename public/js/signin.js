9c61039053e9d99353923b5b0264b147a740d2f8
Spencer <soxe@pugetsound.edu>
2024-12-04 16:24:52 -0800

    bottom of signin.js

--- public/js/signin.js
+++ public/js/signin.js
@@ -22,3 +22,41 @@ document.getElementById('signinform').addEventListener('submit', async (event) =
         }
    }
 });
+
+document.getElementById('signinform').addEventListener('submit', async (event) => {
+    event.preventDefault();
+
+    const username = document.getElementById('username').value;
+    const password = document.getElementById('password').value;
+
+    const hashPass = sha256(password).toString();
+
+    const data = {
+        username: username,
+        password: hashPass
+    };
+
+    try{
+        const response = await fetch('/signingo', {
+            method: 'POST',
+                    headers: {
+                        'Content-Type': 'application/json'
+                    },
+                    body: JSON.stringify(data),
+        });
+
+        if (response.ok){
+            const result = await response.json();
+            if (result.success) {
+                alert("Signed in!");
+                window.location.href = '/homepage';
+            }
+        } else{
+            alert("an error occurred");
+        }
+
+    } catch (error) {
+        console.error('Error:', error);
+        alert("an error occurred");
+    }
+});
